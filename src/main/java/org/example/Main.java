26e41ac7be1efee3e1f3cabae1dfd5bbbfe54f37
Arjunagi A Rehman <abdul123arj@gmail.com>
2023-09-03 14:53:34 +0530

    sunday assignement

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.net.URL;
+
+public class Main {
+    public static void main(String[] args) {
+     //   System.out.println("Hello world!");
+        URL url=null;
+        HttpURLConnection connection = null;
+        int rspon=0;
+        String urlString = "https://api.nationalize.io/?name=nathaniel";
+
+        try {
+            url=new URL(urlString);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            connection=(HttpURLConnection)url.openConnection();
+            rspon=connection.getResponseCode();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if(rspon==200){
+            BufferedReader rawdata=null;
+            try {
+                 rawdata= new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            StringBuilder data=new StringBuilder();
+            String readLine = null;
+
+            while(true)
+            {
+                try {
+                    if ((readLine = rawdata.readLine()) == null) break;
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                data.append(readLine);
+            }
+            System.out.println(data);
+        }
+
+
+    }
+}
